8797a272d08ff34e920adc3f73fbe7b2b50673a6
ricojr1984 <daniel.de.bondt@student.howest.be>
2025-11-24 11:11:57 +0100

    Update map to show in color and include business name

--- components/Map.tsx
+++ components/Map.tsx
@@ -17,15 +17,15 @@ export const Map: React.FC<MapProps> = ({ t }) => {
         </div>
         
         <div className="w-full h-[500px] border-4 border-gold shadow-2xl">
-           <iframe 
-             width="100%" 
-             height="100%" 
-             style={{ border: 0, filter: 'grayscale(100%) contrast(1.2)' }}
-             loading="lazy" 
-             allowFullScreen 
-             referrerPolicy="no-referrer-when-downgrade"
-             src="https://www.google.com/maps?q=Lippenslaan+141,+8300+Knokke-Heist,+Belgium&output=embed">
-           </iframe>
+          <iframe 
+            width="100%" 
+            height="100%" 
+            style={{ border: 0 }}
+            loading="lazy" 
+            allowFullScreen 
+            referrerPolicy="no-referrer-when-downgrade"
+            src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBFw0Qbyq9zTFTd-tUY6dZWTgaQzuU17R8&q=Bistro+Boudoir,Lippenslaan+141,Knokke-Heist,Belgium">
+          </iframe>
         </div>
       </div>
     </section>
